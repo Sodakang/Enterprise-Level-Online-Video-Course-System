1a38191c8321759152a5a33b574cf9ae5cf4c1f8
Sodakang <inlandriver@outlook.com>
2020-06-06 15:57:10 -0700

    Initialize Spring.

---
+++ Online-Course-System/src/test/java/com/project/online_course_system/OnlineCourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.online_course_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
