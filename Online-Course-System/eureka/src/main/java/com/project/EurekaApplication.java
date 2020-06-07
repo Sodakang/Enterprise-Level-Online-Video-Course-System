1ec223dfd847ad95fd3e45f5f254f858e8ed012f
Sodakang <inlandriver@outlook.com>
2020-06-06 17:39:15 -0700

    1. Add some configurations to fix the start of registration center. 2. Add some logs.

--- Online-Course-System/eureka/src/main/java/com/project/EurekaApplication.java
+++ Online-Course-System/eureka/src/main/java/com/project/EurekaApplication.java
@@ -1,13 +1,24 @@
 package com.project;
 
+import freemarker.core.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(EurekaApplication.class);
+		ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
+		LOG.info("Start Successfully!");
+		LOG.info("Eureka Address \thttp://127.0.0.1:{}", environment.getProperty("server.port"));
 	}
 
 }
