01f173245a45861d6a92e3749fe1aabd3b0030e2
Sodakang <inlandriver@outlook.com>
2020-06-14 12:57:40 -0700

    1. Try to use Example, by which we can finish the CRUD works in a single table conveniently.

--- Online-Course-System/server/src/main/java/com/project/server/service/TestService.java
+++ Online-Course-System/server/src/main/java/com/project/server/service/TestService.java
@@ -2,6 +2,7 @@ package com.project.server.service;
 
 import com.project.server.dao.TestMapper;
 import com.project.server.entity.Test;
+import com.project.server.entity.TestExample;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,7 +14,10 @@ public class TestService {
     @Resource
     private TestMapper testMapper;
 
-//    public List<Test> list() {
-//        return testMapper.list();
-//    }
+    public List<Test> list() {
+        TestExample testExample = new TestExample();
+        testExample.createCriteria().andIdEqualTo("1");
+        // testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
+    }
 }
