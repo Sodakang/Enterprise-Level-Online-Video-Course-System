94c8f45275436202b0ea8f2f80a50f736e8fa741
Sodakang <inlandriver@outlook.com>
2020-06-18 20:43:23 -0700

    1. Use mounted to fix the problem in component initialization style.

--- Online-Course-System/admin/src/views/login.vue
+++ Online-Course-System/admin/src/views/login.vue
@@ -75,9 +75,12 @@
 </template>
 
 <script>
-    $('body').attr('class', 'login-layout light-login');
     export default {
         name: 'App',
+        mounted: function() {
+            $('body').removeClass('no-skin');
+            $('body').attr('class', 'login-layout light-login')
+        },
         methods: {
             login() {
                 this.$router.push("/admin")
