e59e7ba396cc9dc9c580e67d6b79561fdd59a6d2
Sodakang <inlandriver@outlook.com>
2020-06-21 12:45:55 -0700

    1. Add SQL scripts for table chapter, and generate its persistence layer code.

--- Online-Course-System/doc/database/all.sql
+++ Online-Course-System/doc/database/all.sql
@@ -1,8 +1,16 @@
-drop table if exists test;
-create table test (
- id char(8) not null default '' comment 'id',
- name varchar(50) comment 'name',
- primary key (id)
-) engine=innodb default charset=utf8mb4 comment='test';
+# drop table if exists test;
+# create table test (
+#  id char(8) not null default '' comment 'id',
+#  name varchar(50) comment 'name',
+#  primary key (id)
+# ) engine=innodb default charset=utf8mb4 comment='test';
+#
+# insert into test (id, name) values (1, 'test');
 
-insert into test (id, name) values (1, 'test');
+drop table if exists `chapter`;
+create table `chapter` (
+    `id` char(8) not null,
+    `course_id` char(8),
+    `name` varchar(50),
+    primary key (`id`)
+) engine = innodb default charset = utf8mb4;
