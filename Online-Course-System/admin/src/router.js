6a8c76a6fbd8706d1bcd0ab326479f4f5cb08123
Sodakang <inlandriver@outlook.com>
2020-06-21 18:27:29 -0700

    1. Modify the routine: remove /admin.

--- Online-Course-System/admin/src/router.js
+++ Online-Course-System/admin/src/router.js
@@ -1,30 +1,33 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from './views/login'
-import Admin from './views/admin'
-import Welcome from './views/admin/welcome'
-import Chapter from './views/admin/chapter'
+import Vue from "vue"
+import Router from "vue-router"
+import Login from "./views/login"
+import Admin from "./views/admin"
+import Welcome from "./views/admin/welcome"
+import Chapter from "./views/admin/chapter"
 
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
+    mode: "history",
     base: process.env.BASE_URL,
     routes: [{
-        path: '*',
+        path: "*",
         redirect: "/login",
     }, {
-        path: '/login',
+        path: "/login",
         component: Login
     }, {
-        path: '/admin',
+        path: "/",
+        name: "admin",
         component: Admin,
         children: [{
-            // the configuration of the subroutine cannot begin with '/'
-            path: 'welcome',
+            // the configuration of the subroutine cannot begin with "/"
+            path: "welcome",
+            name: "welcome",
             component: Welcome
         }, {
-            path: 'business/chapter',
+            path: "business/chapter",
+            name: "business/chapter",
             component: Chapter
         }]
     }]
