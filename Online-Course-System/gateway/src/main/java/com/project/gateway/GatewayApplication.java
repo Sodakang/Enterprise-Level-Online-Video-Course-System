63dd40aeb3f67057d915cf333a585ecb198d96d5
Sodakang <inlandriver@outlook.com>
2020-06-22 19:45:01 -0700

    1. Fix the cross-domain problem in the gateway module instead of the server module.

--- Online-Course-System/gateway/src/main/java/com/project/gateway/GatewayApplication.java
+++ Online-Course-System/gateway/src/main/java/com/project/gateway/GatewayApplication.java
@@ -5,7 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,6 +23,23 @@ public class GatewayApplication {
 		ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
 		LOG.info("Start Successfully!");
 		LOG.info("Gateway Address \thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+	}
+
+	@Bean
+	public CorsWebFilter corsFilter() {
+
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowCredentials(Boolean.TRUE);
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedHeader("*");
+		configuration.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+		source.registerCorsConfiguration("/**", configuration);
+
+		return new CorsWebFilter(source);
 	}
 
 }
