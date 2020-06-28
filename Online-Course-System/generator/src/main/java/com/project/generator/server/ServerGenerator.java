079161283f8fb03fd0abd4e2b6b52be8a0e48fcc
Sodakang <inlandriver@outlook.com>
2020-06-27 17:07:56 -0700

    1. Implement a class FreemarkerUtil to simplify the usage of generator. 2. Add ServerGenerator to generate the back-end codes, including controller, service and dto.

---
+++ Online-Course-System/generator/src/main/java/com/project/generator/server/ServerGenerator.java
@@ -0,0 +1,11 @@
+package com.project.generator.server;
+
+import com.project.generator.util.FreemarkerUtil;
+
+import java.io.IOException;
+
+public class ServerGenerator {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
