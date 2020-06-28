8748982732da9896f2c682cb439880b95f90a145
Sodakang <inlandriver@outlook.com>
2020-06-28 14:57:45 -0700

    1. Develop the generator: integrate the code generator with mybatis generator.

--- Online-Course-System/generator/src/main/java/com/project/generator/server/ServerGenerator.java
+++ Online-Course-System/generator/src/main/java/com/project/generator/server/ServerGenerator.java
@@ -4,7 +4,11 @@ import com.project.generator.util.DbUtil;
 import com.project.generator.util.Field;
 import com.project.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -19,6 +23,8 @@ public class ServerGenerator {
 
     private static String toDtoPath = "server//src//main//java//com//project//server//dto//";
 
+    private static String generatorConfigPath = "src//main//resources//generator//generatorConfig.xml";
+
     /**
      * Get all the Java types and use set to remove the duplicates.
      */
@@ -32,10 +38,28 @@ public class ServerGenerator {
     }
 
     public static void main(String[] args) throws Exception {
-        String Domain = "Section";
-        String domain = "section";
         String module = MODULE;
-        List<Field> fieldList = DbUtil.getColumnByTableName(domain);
+
+        // Generate the first table in the configuration file.
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        // Read the xml file into document.
+        Document doc=reader.read(file);
+        // Get the root element of the xml file.
+        Element rootElement=doc.getRootElement();
+        // Get the context element.
+        Element contextElement = rootElement.element("context");
+        // Define an element for iteration.
+        Element tableElement;
+        // Get the first table element.
+        tableElement=contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        System.out.println("Table: "+tableElement.attributeValue("tableName"));
+        System.out.println("Domain: "+tableElement.attributeValue("domainObjectName"));
+
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName);
         Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
         map.put("Domain", Domain);
@@ -48,12 +72,12 @@ public class ServerGenerator {
         FreemarkerUtil.initConfig("dto.ftl");
         FreemarkerUtil.generator(toDtoPath + Domain + "Dto.java", map);
 
-//        // Generate service.
-//        FreemarkerUtil.initConfig("service.ftl");
-//        FreemarkerUtil.generator(toServicePath + Domain + "Service.java", map);
-//
-//        // Generate controller.
-//        FreemarkerUtil.initConfig("controller.ftl");
-//        FreemarkerUtil.generator(toControllerPath + Domain + "Controller.java", map);
+        // Generate service.
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(toServicePath + Domain + "Service.java", map);
+
+        // Generate controller.
+        FreemarkerUtil.initConfig("controller.ftl");
+        FreemarkerUtil.generator(toControllerPath + Domain + "Controller.java", map);
     }
 }
