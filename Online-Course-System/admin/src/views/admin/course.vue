de3f91a9a063f02d6a0a350bceec8bc125b9e3c5
Sodakang <inlandriver@outlook.com>
2020-07-01 14:52:53 -0700

    1. Beautify the course management front-end.

--- Online-Course-System/admin/src/views/admin/course.vue
+++ Online-Course-System/admin/src/views/admin/course.vue
@@ -11,52 +11,89 @@
                 Refresh
             </button>
         </p>
-        <table id="simple-table" class="table  table-bordered table-hover">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Summary</th>
-                <th>Duration (seconds)</th>
-                <th>Price ($)</th>
-                <th>Cover Image</th>
-                <th>Level</th>
-                <th>Charge or Free</th>
-                <th>Status</th>
-                <th>Enrollment</th>
-                <th>Order</th>
 
+        <div class="row">
+            <div v-for="course in courses" class="col-md-4">
+                <div class="thumbnail search-thumbnail">
+                    <img v-show="!course.image" class="media-object" src="/static/image/demo-course.jpg" />
+                    <img v-show="course.image" class="media-object" v-bind:src="course.image" />
+                    <div class="caption">
+                        <div class="clearfix">
+                            <span class="pull-right label label-primary info-label">
+                                {{COURSE_LEVEL | optionKV(course.level)}}
+                            </span>
+                            <span class="pull-right label label-primary info-label">
+                                {{COURSE_CHARGE | optionKV(course.charge)}}
+                            </span>
+                            <span class="pull-right label label-primary info-label">
+                                {{COURSE_STATUS | optionKV(course.status)}}
+                            </span>
+                        </div>
 
-                <th>Operations</th>
-            </tr>
-            </thead>
-
-            <tbody>
-            <tr v-for="course in courses">
-                <td>{{course.id}}</td>
-                <td>{{course.name}}</td>
-                <td>{{course.summary}}</td>
-                <td>{{course.duration}}</td>
-                <td>{{course.price}}</td>
-                <td>{{course.image}}</td>
-                <td>{{COURSE_LEVEL | optionKV(course.level)}}</td>
-                <td>{{COURSE_CHARGE | optionKV(course.charge)}}</td>
-                <td>{{COURSE_STATUS | optionKV(course.status)}}</td>
-                <td>{{course.enrollment}}</td>
-                <td>{{course.sort}}</td>
-                <td>
-                    <div class="hidden-sm hidden-xs btn-group">
-                        <button v-on:click="edit(course)" class="btn btn-xs btn-info">
-                            <i class="ace-icon fa fa-pencil bigger-120"></i>
-                        </button>
-                        <button v-on:click="del(course.id)" class="btn btn-xs btn-danger">
-                            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                        </button>
+                        <h3 class="search-title">
+                            <a href="#" class="blue">{{course.name}}</a>
+                        </h3>
+                        <p>
+                            <span class="blue bolder bigger-150">{{course.price}}&nbsp;<i class="fa fa-dollar"></i></span>&nbsp;
+                        </p>
+                        <p>{{course.summary}}</p>
+                        <p>
+                            <button v-on:click="edit(course)" class="btn btn-xs btn-info">
+                                <i class="ace-icon fa fa-pencil bigger-120"></i>
+                            </button>
+                            <button v-on:click="del(course.id)" class="btn btn-xs btn-danger">
+                                <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                            </button>
+                        </p>
                     </div>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+                </div>
+            </div>
+        </div>
+
+<!--        <table id="simple-table" class="table  table-bordered table-hover">-->
+<!--            <thead>-->
+<!--            <tr>-->
+<!--                <th>ID</th>-->
+<!--                <th>Name</th>-->
+<!--                <th>Summary</th>-->
+<!--                <th>Duration (seconds)</th>-->
+<!--                <th>Price ($)</th>-->
+<!--                <th>Cover Image</th>-->
+<!--                <th>Level</th>-->
+<!--                <th>Charge or Free</th>-->
+<!--                <th>Status</th>-->
+<!--                <th>Enrollment</th>-->
+<!--                <th>Order</th>-->
+<!--                <th>Operations</th>-->
+<!--            </tr>-->
+<!--            </thead>-->
+
+<!--            <tbody>-->
+<!--            <tr v-for="course in courses">-->
+<!--                <td>{{course.id}}</td>-->
+<!--                <td>{{course.name}}</td>-->
+<!--                <td>{{course.summary}}</td>-->
+<!--                <td>{{course.duration}}</td>-->
+<!--                <td>{{course.price}}</td>-->
+<!--                <td>{{course.image}}</td>-->
+<!--                <td>{{COURSE_LEVEL | optionKV(course.level)}}</td>-->
+<!--                <td>{{COURSE_CHARGE | optionKV(course.charge)}}</td>-->
+<!--                <td>{{COURSE_STATUS | optionKV(course.status)}}</td>-->
+<!--                <td>{{course.enrollment}}</td>-->
+<!--                <td>{{course.sort}}</td>-->
+<!--                <td>-->
+<!--                    <div class="hidden-sm hidden-xs btn-group">-->
+<!--                        <button v-on:click="edit(course)" class="btn btn-xs btn-info">-->
+<!--                            <i class="ace-icon fa fa-pencil bigger-120"></i>-->
+<!--                        </button>-->
+<!--                        <button v-on:click="del(course.id)" class="btn btn-xs btn-danger">-->
+<!--                            <i class="ace-icon fa fa-trash-o bigger-120"></i>-->
+<!--                        </button>-->
+<!--                    </div>-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            </tbody>-->
+<!--        </table>-->
         <!-- Modal -->
         <div class="modal fade" id="form-modal" tabindex="-1" role="dialog" aria-labelledby="form-modal-label">
             <div class="modal-dialog" role="document">
@@ -259,3 +296,9 @@
         }
     }
 </script>
+
+<style scoped>
+    .caption h3 {
+        font-size: 20px;
+    }
+</style>
